beecfcb04109bea2c7a9c2ca10f4ee312298fac8
Mary Otto <ottomc@bc.edu>
2019-06-23 16:12:33 +0100

    Initial Commit

---
+++ World Wide Wallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  World Wide Wallet
+//
+//  Created by Mary Otto on 6/23/19.
+//  Copyright © 2019 Mary Otto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
